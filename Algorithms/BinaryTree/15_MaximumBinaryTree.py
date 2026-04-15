a114b4d69bebbc318f0a0a09dab7fa3e6e91feed
M0B <zom817816@gmail.com>
2026-04-15 22:04:21 +0800

    refactor: 移除未使用的deque导入

--- Algorithms/BinaryTree/15_MaximumBinaryTree.py
+++ Algorithms/BinaryTree/15_MaximumBinaryTree.py
@@ -18,7 +18,6 @@
 """
 
 from typing import Optional, List
-from collections import deque
 
 class TreeNode:
     def __init__(self, val: int = 0, left: Optional['TreeNode'] = None, right: Optional['TreeNode'] = None):
